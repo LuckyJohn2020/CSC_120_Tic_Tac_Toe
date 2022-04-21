041a760ea3e8df1c04e82967b9be1daeeea61ddf
Jonathan Alvarado <jonathanjrmusic@gmail.com>
2022-04-20 21:53:27 -0400

    Added 3*3 board and displayed

--- board.py
+++ board.py
@@ -1,2 +1,19 @@
-## Jonathan Alvardo
-## April 18th, 2022
+# Jonathan Alvarado
+# April 18th, 2022
+
+new_board = ["[-", "-", "-]",
+             "[-", "-", "-]",
+             "[-", "-", "-]"]
+
+
+def board_displayed():
+    print(new_board[0] + " | " + new_board[1] + " | " + new_board[2])
+    print(new_board[3] + " | " + new_board[4] + " | " + new_board[5])
+    print(new_board[6] + " | " + new_board[7] + " | " + new_board[8])
+
+
+def start_game():
+    board_displayed()
+
+
+start_game()
