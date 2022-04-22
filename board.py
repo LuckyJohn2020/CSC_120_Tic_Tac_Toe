ca8a821c24ffd9c85f617a0f0bca73c6248fe590
Jonathan Alvarado <jonathanjrmusic@gmail.com>
2022-04-21 21:11:25 -0400

    reworked the code based on group to be more efficient

--- board.py
+++ board.py
@@ -1,19 +1,18 @@
 # Jonathan Alvarado
 # April 18th, 2022
 
-new_board = ["[-", "-", "-]",
-             "[-", "-", "-]",
-             "[-", "-", "-]"]
+board = [['-', '-', '-'],
+         ['-', '-', '-'],
+         ['-', '-', '-']]
 
 
-def board_displayed():
-    print(new_board[0] + " | " + new_board[1] + " | " + new_board[2])
-    print(new_board[3] + " | " + new_board[4] + " | " + new_board[5])
-    print(new_board[6] + " | " + new_board[7] + " | " + new_board[8])
+def main():
+    print_board()
 
 
-def start_game():
-    board_displayed()
+def print_board():
+    for i in board:
+        print(i)
 
 
-start_game()
+main()
